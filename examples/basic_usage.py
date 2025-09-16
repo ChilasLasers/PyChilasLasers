04ed9a1f2609f713ce54610f25f4940e7bb1eebf
Albert van Rees <a.vanrees@chilasbv.com>
2025-09-16 14:37:03 +0200

    docs: clarify demo example

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -2,15 +2,15 @@
 """
 Demo script for pychilaslasers library.
 
-This demo script follows a typical usage of the library. It initializes a laser, powers it on
-performs some common operations with it before safely shutting it down.
+This demo script follows a typical usage of the library. It initializes a laser, powers it on,
+performs some common operations, before safely shutting it down.
 """
 
 import logging
 from pathlib import Path
 from pychilaslasers.modes import LaserMode
 from pychilaslasers import Laser
-from  basic_usage_sweeping import run_sweeping_example,select_com_port
+from basic_usage_sweeping import run_sweeping_example, select_com_port
 
 
 # Change logging level to change the verbosity of the terminal output.
@@ -18,45 +18,44 @@ from  basic_usage_sweeping import run_sweeping_example,select_com_port
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+com_port: str = select_com_port()
 
-address: str = select_com_port()
-# Path to calibration file/LookUpTable (lut)
-fp_lut = Path("path/to/file")
-
+# Path to *.csv file, which contains the calibration Look-Up Table (lut)
+path_calibration_lut = Path("path/to/file")
 
 # Initiate laser object
-laser = Laser(com_port=address, calibration_file=fp_lut)
+laser = Laser(com_port=com_port, calibration_file=path_calibration_lut)
 
-# Check connection state
+# Check laser connection
 print(f"Connection state {laser.system_state}")
 
-# Turn on system
+# Turn on laser system
 laser.system_state = True
 
-# Steady tuning functionality
+# Laser switches to tuning mode for wavelength tuning (applies to both COMET and ATLAS lasers)
 laser.mode = LaserMode.STEADY
 
-# Set wavelength, e.g. 1550.000nm
-laser.steady.wavelength = 1598.000
+# Set laser wavelength to any value within the tuning range given by the calibration look-up table
+laser.steady.wavelength = 1598.000  # nm
 
-# Get current wavelength
-print(f"Current wavelength is {laser.steady.wavelength}")
+# Get current laser wavelength
+print(f"Current wavelength is {laser.steady.wavelength} nm")
 input("Press enter to continue...")
 
-# Set wavelength in relative manner (apply step/offset), e.g. increase wavelength by 0.004nm
-print(f"New wavelength is {laser.steady.set_wl_relative(0.004)}")
+# Set wavelength in relative manner to apply a single step or offset
+print(f"New wavelength is {laser.steady.set_wl_relative(0.004)} nm")
 
-# ... or decrease wavelength with negative step, e.g. -1.000nm
-print(f"New wavelength is {laser.steady.set_wl_relative(-1.000)}")
+# ... or decrease wavelength with a negative step, e.g. -1.000 nm
+print(f"New wavelength is {laser.steady.set_wl_relative(-1.000)} nm")
 
 input("Press Enter to continue...")
 
-# Emit a trigger pulse 
+# Emit a trigger pulse for synchronization with other equipment, e.g. to indicate that the wavelength is set
 laser.trigger_pulse()
 
-
+# Laser continues with sweeping example (only applies to COMET lasers)
 if laser.model == "COMET":
     run_sweeping_example(laser) 
 
-# Turn off the laser
+# Finally, turn off the laser
 laser.system_state = False
--- examples/basic_usage_sweeping.py
+++ examples/basic_usage_sweeping.py
@@ -11,7 +11,8 @@ from pathlib import Path
 from pychilaslasers import Laser
 
 
-fp_lut = Path("path/to/file")
+# Path to *.csv file, which contains the calibration Look-Up Table (lut)
+path_calibration_lut = Path("path/to/file")
 
 # Change logging level to change the verbosity of the terminal output.
 # Debug will print all the serial commands and responses sent to the laser.
@@ -19,42 +20,46 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def select_com_port() -> str: 
+def select_com_port() -> str:
     from pychilaslasers import utils
-    if len(ports:= utils.list_comports()) == 1:
-        address: str = ports[0]
+    if len(ports := utils.list_comports()) == 1:
+        selected_com_port: str = ports[0]
     elif len(ports) < 0:
         print("No COM ports found. Please connect the laser and try again.")
         exit(1)
     else:
-        while True: 
-            address = input(f"Available COM ports: {ports}\nPlease enter the COM port address: ")
-            if address in ports:
+        while True:
+            selected_com_port = input(f"Available COM ports: {ports}\nPlease enter the COM port address: ")
+            if selected_com_port in ports:
                 break
             print("Invalid COM port address. Please choose from.")
-
-    return address
+    return selected_com_port
 
 
 def run_sweeping_example(laser:Laser | None = None) -> None:
-    if laser is None:
-        laser = Laser(calibration_file=fp_lut, com_port=select_com_port())
-
     # Continue with the sweeping example using the laser object
+    if laser is None:
+        laser = Laser(calibration_file=path_calibration_lut, com_port=select_com_port())
+
+    # Turn on laser system
     laser.system_state = True
+
+    # Laser switches to sweep mode for wavelength sweeping (applies only to COMET lasers)
     laser.mode = "sweeping"
+
+    # Automatically define the sweep range for a full sweep, based on the maximum tuning range of the calibration look-up table
     start_wavelength: float = laser.sweep.start_wavelength
     end_wavelength: float = laser.sweep.end_wavelength
-    
+    print(f"Starting sweep from {start_wavelength} nm to {end_wavelength} nm")
 
-
-    laser.sweep.interval = int(input("Enter the sweep interval (in us): "))
-    print(f"Starting sweep from {start_wavelength}nm to {end_wavelength}nm")
+    # Start a full wavelength sweep for an infinite number of sweeps, until it is interrupted by stop()
     laser.sweep.start()
 
+    # Stop wavelength sweep
     input("Press Enter to stop the sweep...")
     laser.sweep.stop()
 
+    # Manually define the sweep range
     print(f"Now sweeping with user-defined bounds. Min is {start_wavelength}nm, Max is {end_wavelength}nm")
     low_bound = input("Enter the start wavelength (nm): ")
     high_bound = input("Enter the end wavelength (nm): ")
@@ -65,7 +70,10 @@ def run_sweeping_example(laser:Laser | None = None) -> None:
     except ValueError as e:
         print(f"Error setting bounds: {e}")
 
+    # Start a full wavelength sweep for an infinite number of sweeps, until it is interrupted by stop()
     laser.sweep.start()
+
+    # Stop wavelength sweep
     input("Press Enter to stop the sweep...")
     laser.sweep.stop()    
 
@@ -73,7 +81,3 @@ def run_sweeping_example(laser:Laser | None = None) -> None:
 if __name__ == "__main__":
     # Example usage
     run_sweeping_example()
-
-
-
-
