7a1e5c52f68dd3411d5f5f5857eb367729e01b82
Albert van Rees <a.vanrees@chilasbv.com>
2025-09-16 16:33:45 +0200

    fix(laser): import sorting

--- src/pychilaslasers/laser.py
+++ src/pychilaslasers/laser.py
@@ -30,6 +30,8 @@ if TYPE_CHECKING:
 # ✅ Standard library imports
 import logging
 
+# ✅ Local imports
+from pychilaslasers.modes.sweep_mode import SweepMode
 from pychilaslasers.comm import Communication
 from pychilaslasers.exceptions.mode_error import ModeError
 from pychilaslasers.laser_components.diode import Diode
@@ -37,9 +39,6 @@ from pychilaslasers.laser_components.tec import TEC
 from pychilaslasers.modes.manual_mode import ManualMode
 from pychilaslasers.modes.mode import LaserMode, Mode
 from pychilaslasers.modes.steady_mode import SteadyMode
-
-# ✅ Local imports
-from pychilaslasers.modes.sweep_mode import SweepMode
 from pychilaslasers.utils import read_calibration_file
 
 logger: logging.Logger = logging.getLogger(__name__)
